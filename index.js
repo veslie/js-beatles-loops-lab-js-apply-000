d84d3870a7e8ff5511ad9e9eadf3830308497893
veslie losius <veslielosius@yahoo.com>
2019-01-17 16:51:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-function theBeatlesplay (string)
-{
-var instruments, musicians = (string)
-
- return theBeatlesPlays; '[ "John Lennon","Paul McCartney", "Guitar", "Bass Guitar" "", ]!!!'
-}
+function theBeatlesplay (musicians, instruments) {
+  //since musicians and instruments are included as parameter names, you can reference them inside the function definition to get access to the array that are passed in when the function is called
+  }
