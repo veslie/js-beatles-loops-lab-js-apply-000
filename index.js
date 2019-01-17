8d12fa1a89fcc1457fab38bfbb23732206ec1c98
veslie losius <veslielosius@yahoo.com>
2019-01-17 16:33:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var instruments, musicians = (string)
 
  return theBeatlesPlays; '[ "John Lennon","Paul McCartney", "Guitar", "Bass Guitar" "", ]!!!'
 }
-int x = 0;
+int; x = 0;
 while (x , width) {
   line(x, 0, x, height);
   x = + 20;
-for (int; y = 0; y < height; y = y + 20) { line(0; y, width, y);
+  }
+ for; '(' int; y = 0; y < height; y = y + 20')' { line(0; y, width, y);
 }
