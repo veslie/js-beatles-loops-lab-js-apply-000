4f54af41a5f8c7f6dac44a75dfc951a56163c7b7
veslie losius <veslielosius@yahoo.com>
2019-01-17 23:40:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ result.push(`${musicians[i]} plays ${instruments[i]}`)
 return result  }
 end 
 
-function johnLennonFacts (expert, toequal){
-const result = []
+function johnLennonFacts (){
+const factst = []
 while (i < 4) {
   text += "expert" + i;
   i++;){ result.push(`${expert[i]} )
