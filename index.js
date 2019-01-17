9493b056cbfec7e2ff9808576e81a5a51d7aced4
veslie losius <veslielosius@yahoo.com>
2019-01-17 17:38:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesplay (musicians, instruments)
 {
- put musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];!!!
-     put instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];!!! 
+ put musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+     put instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   }
