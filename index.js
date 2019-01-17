8096505b1c8629553da0eea5f1c12d394684278b
veslie losius <veslielosius@yahoo.com>
2019-01-17 15:52:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function theBeatlesplay() 
+function theBeatlesplay {
 var instruments = ()
-{
+var musicians = ()
+
  return theBeatlesPlays.["John Lennon", "Paul McCartney", "Guitar", "Bass Guitar" ]
 }
