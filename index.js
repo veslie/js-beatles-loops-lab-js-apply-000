3d003e96ad0b2f76a5287bce20cf18b3b123a875
veslie losius <veslielosius@yahoo.com>
2019-01-17 16:35:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ while (x , width) {
   line(x, 0, x, height);
   x = + 20;
   }
- for (' int; y = 0; y < height; y = y + 20' { line(0; y, width, y);
+ for (' int; y = 0; y < height; y = y + 20' line(0; y, width, y);
 }
