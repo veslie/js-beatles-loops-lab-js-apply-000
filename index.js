63a275d27a66713c45c53547bfb5455fe74f8fdf
veslie losius <veslielosius@yahoo.com>
2019-01-17 17:33:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function theBeatlesplay (musicians, instruments)
 {
-  //since musicians and instruments are included as parameter names, you can reference them inside the function definition to get access to the array that are passed in when the function is called
+  
   }
