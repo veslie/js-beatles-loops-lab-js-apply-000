8e55ff7896105260855b6c2601cc3e3b55a48acb
veslie losius <veslielosius@yahoo.com>
2019-01-17 17:46:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function theBeatlesplay (musicians, instruments)
 {
- put; musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-     put; instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+ 
   }
