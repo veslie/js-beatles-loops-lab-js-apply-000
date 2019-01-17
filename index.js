d405df57291963e1579f4c6be3e904a0f6f5ffe3
veslie losius <veslielosius@yahoo.com>
2019-01-17 23:02:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function theBeatlesplay (musicians, instruments){
+function theBeatlesPlay (musicians, instruments){
 const result = []
 for (var i = 0; i < musicians.length; i++) { 
 result.push(`${musicians[0]}plays ${instruments[1]}`)
